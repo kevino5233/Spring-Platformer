837bd1d795f7e25f53f43845491d84c571d4b28a
Chindianteh <chindianteh@gmail.com>
2014-11-15 15:37:29 -0600

    Added models and level scene.

---
+++ Assets/PlayerFollower.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerFollower : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = GameObject.FindGameObjectWithTag("Player").transform.position + new Vector3(0, 30, -30);
+	}
+}
---
+++ Assets/RodMovement.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RodMovement : MonoBehaviour {
+
+	public GameObject destination;
+	public float errorMargin;
+
+	private Transform target;
+	private Transform start;
+	private bool toTarget;
+	// Use this for initialization
+	void Start () {
+		target = destination.transform;
+		start = transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 theForce;
+		if(toTarget){
+			theForce = target.position - transform.position;
+			rigidbody.AddForce(theForce);
+			if(Vector3.SqrMagnitude(theForce) < errorMargin){
+				toTarget = false;
+			}
+		}
+		else{
+			theForce = start.position - transform.position;
+			rigidbody.AddForce(theForce);
+			if(Vector3.SqrMagnitude(theForce) < errorMargin){
+				toTarget = true;
+			}
+		}
+	}
+}
