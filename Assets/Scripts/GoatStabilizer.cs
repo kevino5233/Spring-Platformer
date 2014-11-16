60d63e9b77bb5053fb29cde13375651372198087
Kaivan Wadia <kaivanwadia@gmail.com>
2014-11-16 09:52:29 -0600

    Merging and refactoring

---
+++ Assets/Scripts/GoatStabilizer.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GoatStabilizer : MonoBehaviour {
+
+	Vector3 frozenPos = Vector3.zero;
+
+	bool frozen = false;
+	int framesFrozenMax = 120;
+	int framesFrozenElapsed = 120;
+
+	void Update () {
+		Vector3 angle = this.rigidbody.transform.eulerAngles;
+		angle.x = 90;
+		this.rigidbody.transform.eulerAngles = angle;
+		if (frozen && framesFrozenElapsed++ < framesFrozenMax){
+			this.rigidbody.transform.position = frozenPos;
+		}
+	}
+	
+	public void Freeze(){
+		frozen = true;
+		framesFrozenElapsed = 0;
+		frozenPos = this.rigidbody.transform.position;
+	}
+}
