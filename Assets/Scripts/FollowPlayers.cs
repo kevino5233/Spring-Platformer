34307b5a39eb077b1c47d722c5514f801f14329d
Kaivan Wadia <kaivanwadia@gmail.com>
2014-11-16 16:06:29 -0600

    Final before presentation

--- Assets/Scripts/FollowPlayers.cs
+++ Assets/Scripts/FollowPlayers.cs
@@ -48,6 +48,9 @@ public class FollowPlayers : MonoBehaviour
 		{
 			return;
 		}
+		PlayerPrefs.SetString ("lastlevel", Application.loadedLevelName);
+		GameObject.Find ("Camera").GetComponent<AudioSource> ().Stop ();
+		GameObject.Find ("GameEndSound").GetComponent<AudioSource> ().Play ();
 		float p0score = 0;
 		float p1score = 0;
 		p0score = GameObject.Find ("Player0").GetComponent<PlayerController> ().health + GameObject.Find ("Player0").GetComponent<Player> ().Score;
@@ -74,19 +77,10 @@ public class FollowPlayers : MonoBehaviour
 			PlayerPrefs.SetString("winner", "lose");
 			Invoke ("NextLevel", 1);
 		}
-<<<<<<< HEAD
 	}
 	
 	void NextLevel(){
 		Application.LoadLevel("LevelEndScreen");
-=======
-		PlayerPrefs.SetString ("winner", winner);
-		PlayerPrefs.SetString ("lastlevel", Application.loadedLevelName);
-		GameObject.Find ("Camera").GetComponent<AudioSource> ().Stop ();
-		GameObject.Find ("GameEndSound").GetComponent<AudioSource> ().Play ();
-		Debug.Log (GameObject.Find ("GameEndSound").GetComponent<AudioSource> ());
-		Invoke ("EndLevel", 1);
->>>>>>> 7334dedab9d5b04883cb366ade86f9c813c5247e
 	}
 
 	void EndLevel()
